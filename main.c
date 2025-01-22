df4ecbf4d2358350e6523848fc670b3dcd1392ef
Matthew Reynolds <9809557+matt-re@users.noreply.github.com>
2025-01-22 19:30:30 +0000

    Formatting

--- main.c
+++ main.c
@@ -13,12 +13,16 @@ int
 main(int argc, char *argv[])
 {
 	FILE *file = fopen(argc > 1 ? argv[1] : "hexdump.h", "rb");
-	if (!file) return 1;
+	if (!file) {
+		return 1;
+	}
 	char buf[512];
 	uintptr_t whence = 0;
 	for (;;) {
 		size_t n = fread(buf, sizeof *buf, sizeof buf, file);
-		if (n == 0) break;
+		if (n == 0) {
+			break;
+		}
 		hexdump(buf, n, whence, print);
 		whence += n;
 	}
